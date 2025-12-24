be8dcd0f046d11bcb5d05b359090cb2bbdb3834b
Anna <gipcha2000@gmail.com>
2025-12-24 05:04:26 -0800

    Lesson 2: variables, expressions, console.log

---
+++ JavaScript/script.js
@@ -0,0 +1,14 @@
+// 1. Создаем переменные для города и температуры
+let city = "Yalta";
+let temperature = "3";
+
+// Выводим информацию о городе в консоль
+console.log ("City", city);
+console.log ("Air temperature", temperature + "°C");
+
+// 2. Выражение: сумма двух чисел и умножение на третье
+let result = (25 + 76) * 3;
+
+// Выводим результат вычисления
+console.log ("Result:", result);
+
