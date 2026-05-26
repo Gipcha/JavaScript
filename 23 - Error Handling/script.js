a9093cc1adf3d6a3a12090442cbe4c9ac65aced9
Anna <gipcha2000@gmail.com>
2026-05-26 13:55:39 -0700

    edited file name

--- 23 - Error Handling/script.js
+++ 23 - Error Handling/script.js
@@ -2,44 +2,43 @@
      Если второй аргумент равен нулю, функция должна бросать ошибку с сообщением "Деление на ноль невозможно".
      Используй `try...catch` для обработки ошибок и выведи сообщение об ошибке в консоль;*/
 
-     function safeDivide (a, b){
-        if (b === 0) {
-            throw new Error('Деление на ноль невозможно!');
-        }
-        return a / b;
-     }
-     try {
-        console.log(safeDivide(10, 0));
-     } catch (error) {
-        console.log('Ошибка:', error.message);
-     }
-    
+function safeDivide(a, b) {
+  if (b === 0) {
+    throw new Error("Деление на ноль невозможно!");
+  }
+  return a / b;
+}
+try {
+  console.log(safeDivide(10, 0));
+} catch (error) {
+  console.log("Ошибка:", error.message);
+}
+
 /*2. Напиши функцию `transfromJSON`, которая принимает строку в формате JSON и возвращает объект.
      Используй `try...catch` для обработки возможных ошибок при парсинге JSON и выведи сообщение об ошибке в консоль;*/
 
-     function transfromJSON (str){
-       try { 
-        return JSON.parse(str);
-       } catch (error){
-        console.log('Ошибка парсинга JSON:', error.message);
-        return null;
-       }
-     }
-     console.log(transfromJSON('{"name":"Anna","age":25}'));
-
+function transformJSON(str) {
+  try {
+    return JSON.parse(str);
+  } catch (error) {
+    console.log("Ошибка парсинга JSON:", error.message);
+    return null;
+  }
+}
+console.log(transformJSON('{"name":"Anna","age":25}'));
 
 /*3. Напиши функцию `checkAccess`, которая принимает возраст пользователя. Если возраст меньше 18,
      функция должна бросать ошибку с сообщением "Доступ запрещен".
      Используйте `try...catch` для обработки ошибок и выведи сообщение об ошибке в консоль.*/
 
-      function checkAccess (age){ 
-         if(age < 18) {
-         throw new Error('возраст меньше 18');
-      }
-      return age
-   }
-     try {
-    console.log(checkAccess(10));
+function checkAccess(age) {
+  if (age < 18) {
+    throw new Error("Доступ запрещен");
+  }
+  return age;
+}
+try {
+  console.log(checkAccess(10));
 } catch (error) {
-    console.log('Доступ запрещен:', error.message); 
+  console.log("Доступ запрещен", error.message);
 }
