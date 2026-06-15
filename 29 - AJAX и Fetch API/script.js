9438e34cd1d6b34d9bd4dc2414c41bd5d320c226
Anna <gipcha2000@gmail.com>
2026-06-14 17:31:46 -0700

    add PUT request for updating posts

--- 29 - AJAX и Fetch API/script.js
+++ 29 - AJAX и Fetch API/script.js
@@ -56,10 +56,8 @@ const showListBtn = document.getElementById("show-list-btn");
 const postsList = document.getElementById("posts-list");
 
 showListBtn.addEventListener("click", () => {
-  // сброс состояния + подготовка UI
   postsList.classList.add("active");
   postsList.innerHTML = "<p>Loading...</p>";
-
   fetch("https://jsonplaceholder.typicode.com/posts")
     .then((response) => response.json())
     .then((posts) => {
@@ -71,12 +69,22 @@ showListBtn.addEventListener("click", () => {
             <p><b>ID:</b> ${post.id}</p>
             <p><b>Title:</b> ${post.title}</p>
             <p><b>Body:</b> ${post.body}</p>
+            <button data-id="${post.id}">Delete</button>
           </div>
         `;
       });
-
       postsList.innerHTML = html;
+      const buttons = postsList.querySelectorAll("button");
+      buttons.forEach((btn) => {
+        btn.addEventListener("click", () => {
+          const id = btn.dataset.id;
+
+          deleteIdPost(id);
+          btn.parentElement.remove();
+        });
+      });
     })
+
     .catch(() => {
       postsList.innerHTML = "<p>Error loading posts</p>";
     });
@@ -84,6 +92,60 @@ showListBtn.addEventListener("click", () => {
 
 //3. Создай функцию для удаления поста по id. Выполни DELETE запрос к API и обнови DOM, удалив соответствующий пост;
 
+function deleteIdPost(id) {
+  fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+    method: "DELETE",
+  });
+}
+
 //4. Реализуй функциональность для обновления данных пользователя.
 //   Используй PUT запрос для отправки обновленных данных на сервер и отобрази обновленный профиль на странице.
 //   Объясни, в чём разница между PUT и PATCH запросами.
+
+const updateBtn = document.getElementById("update-btn");
+
+updateBtn.addEventListener("click", () => {
+  const id = userId.value;
+  if (!userId.value || !title.value || !body.value) {
+    serverResponse.style.display = "block";
+    serverResponse.innerHTML = `
+      <div class="error">Please fill in all fields</div>
+    `;
+    return;
+  }
+  fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+    body: JSON.stringify({
+      userId: userId.value,
+      title: title.value,
+      body: body.value,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      serverResponse.style.display = "block";
+
+      serverResponse.innerHTML = `
+        <div class="success">POST UPDATED SUCCESSFULLY</div>
+
+        <div class="post-card">
+          <p><b>ID:</b> ${data.id}</p>
+          <p><b>User ID:</b> ${data.userId}</p>
+          <p><b>Title:</b> ${data.title}</p>
+          <p><b>Body:</b> ${data.body}</p>
+        </div>
+      `;
+
+      userId.value = "";
+      title.value = "";
+      body.value = "";
+    });
+});
+/*PUT и PATCH отличаются тем, как они обновляют данные на сервере.
+  PUT — полностью заменяет ресурс.
+  Мы отправляем объект целиком и старые данные переписываются новыми.
+  PATCH — обновляет только часть ресурса.
+  Мы отправляем только те поля, которые нужно изменить, остальные остаются без изменений.*/
